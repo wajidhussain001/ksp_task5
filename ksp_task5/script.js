68c5189b733ea151b0a0d22587cff7eb69bfdcc1
wajidhussain001 <147056326+wajidhussain001@users.noreply.github.com>
2023-10-11 17:15:27 +0530

    Add files via upload

---
+++ ksp_task5/script.js
@@ -0,0 +1,161 @@
+// comments
+window.alert("Welcome To My Website");
+console.log("Hello World");
+
+// Variables
+
+let var1 = 5;
+let var2 = 7;
+
+let nameOfAPerson = "king";//CamelCase
+let ageOfAPerson = 30;
+
+console.log(var1);
+console.log(nameOfAPerson);
+console.log("ageOfAPerson"); 
+
+//Constant
+const months = 12;
+
+console.log(months);
+
+//Operators
+
+// arithemetic op = -,=,*,/
+// assignments op =
+// comparision op
+// logical op
+
+
+// arithemetic op
+
+let number1 = 4;
+let number2 = 2;
+
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);//remainder
+console.log(number1**number2);//power
+
+// increment //decrement
+
+//increment
+
+let age = 10;
+age++;
+
+console.log(age);
+
+//Decrement
+let no = 15;
+no--;
+
+console.log(no);
+
+// assignment op  =,+=,-=,*=,/=,%=,**=
+
+let berries = 13;
+berries += 5;
+
+console.log(berries);
+
+let cherries = 13;
+cherries -= 5;
+
+console.log(cherries);
+
+let apple = 2;
+apple *= 5;
+
+console.log(apple);
+
+let pie = 10;
+pie /= 5;
+
+console.log(pie);
+
+let choco = 6;
+choco %= 3;
+
+console.log(choco);
+
+let cake = 4;
+cake **= 2;
+
+console.log(cake);
+
+// comparision op    ==,===,!=,<,>,<=,>=
+
+let number4 = 14;
+let number5 = 14;
+
+console.log(number4==number5);
+
+let number6 = 4;
+let number7 = 4;
+
+console.log(number6===number7);
+
+let number8 = 1;
+let number9 = 1;
+
+console.log(number8!=number9);
+
+let number10 = 2;
+let number11 = 1;
+
+console.log(number10<number11);
+
+let number12 = 2;
+let number13 = 1;
+
+console.log(number10>number11);
+
+let number14 = 2;
+let number15 = 1;
+
+console.log(number14<=number15);
+
+let number16 = 2;
+let number17 = 1;
+
+console.log(number10>=number11);
+
+//data types
+// number
+// boolean - true , false 
+// string - "car","bike",
+// "22" - string
+
+let number18 = 88;
+let number19 = 88;
+
+console.log(number18 ==number19);
+
+let number20 = 88;
+let number21 = 88;
+
+console.log(number20===number21);
+
+let age1 = 44; //number
+let age2 = "44"; //string
+
+console.log(age1==age2);//true
+console.log(age1===age2);//false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
